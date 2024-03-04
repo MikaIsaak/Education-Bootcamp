b9d04ba2392c001b52d1d8c4c417b922a92312bc
MikaIsaak <michaelisayan@gmail.com>
2024-03-04 16:13:30 +0300

    ERC6551

---
+++ contracts/ERC6551/ERC6551NFT.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC6551NFT is ERC721, Ownable {
+    constructor() ERC721("ERC6551NFT", "ERC") Ownable(msg.sender) {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
